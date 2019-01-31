33ef40fbdc10cdafa80ad3d446195fd06ce42c8b
Dennis Heimbigner <dmh@ucar.edu>
2019-01-31 11:19:38 -0700

    debug

--- oc2/ocdebug.h
+++ oc2/ocdebug.h
@@ -26,7 +26,7 @@
    to catch the failure. Turing it on incurs a significant
    performance penalty, so it is off by default.*/
 
-#undef OCCATCHERROR
+#define OCCATCHERROR
 
 #define OCPANIC(msg) assert(ocpanic(msg))
 #define OCPANIC1(msg,arg) assert(ocpanic(msg,arg))
